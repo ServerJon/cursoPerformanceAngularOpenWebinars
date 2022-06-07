e307100624fa4c1a2e0265a329e67be78c127b15
ServerJon <ctsesiphon@hotmail.com>
2022-06-07 07:32:59 +0200

    initial commit

---
+++ apps/world-bank/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'curso-performance-angular-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'world-bank';
+}
